8a64576fa0263818b63f3cbf283cd6c7facadfe8
Selcuk tuy <selcuktuy@gmail.com>
2022-05-15 23:33:01 +0300

    index1.gün

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-meraba birinci ekleme
-ikinci ekleme
-üçüncü ekleme
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css/ustbilgi.css" rel="stylesheet" type="text/css"/>
+    <link href="css/container.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <div class="ustbilgicontainer"> 
+        <div class="ustbilgi" ><img style="margin-left:30px;" src="resim/st.png"></div>
+        <div class="ustbilgi" style="background-color: blue;">
+                <div class="urun";><h1 >ÜRÜN:%S</h1></div>
+                <div class="sablon";><h1 >ŞABLONLAR</h1> </div>
+                <div class="kaynak";><h1 >KAYNAKLAR</h1> </div>
+            </div>
+        <div class="ustbilgi"></div>
+    </div>
+    
+</body>
+</html>
